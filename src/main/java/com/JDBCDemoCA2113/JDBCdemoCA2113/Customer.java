57484c479543644b15e4c53848a7cd2c7f0f098d
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-18 08:37:49 +0300

    Initial commit

---
+++ src/main/java/com/JDBCDemoCA2113/JDBCdemoCA2113/Customer.java
@@ -0,0 +1,12 @@
+package com.JDBCDemoCA2113.JDBCdemoCA2113;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer { //model
+    //variables
+    private int id;
+    private String  name;
+}
---
+++ src/test/java/com/JDBCDemoCA2113/JDBCdemoCA2113/JdbCdemoCa2113ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JDBCDemoCA2113.JDBCdemoCA2113;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbCdemoCa2113ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
